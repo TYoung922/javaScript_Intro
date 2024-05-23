86b507d8ac3adf560b70349e679c205a914c12ca
TercanoNuruva <thyoung89@gmail.com>
2024-05-23 15:02:15 -0600

    finished first part

--- main.js
+++ main.js
@@ -1,57 +1,67 @@
-// single element selectors
-// console.log(document.getElementById("my-form"));
-// console.log(document.querySelector("h1"));
+// const name = "tyler";
+// const age = 35;
 
-//multiple element selectors
-// console.log(document.querySelectorAll(".item"));
-// console.log(document.getElementsByClassName("item"));
-// console.log(document.getElementsByTagName("li"));
+// console.log("My name is " + name + " and I am " + age);
 
-// const items = document.querySelectorAll(".item");
+// console.log(`My name is ${name} and I am ${age}`);
+// const fruits = ["apples", "oranges", "pears"];
 
-// items.forEach((item) => console.log(item));
+// console.log(fruits);
+// console.log(fruits[1]);
+// fruits[3] = "grapes";
 
-// const ul = document.querySelector(".items");
+// console.log(fruits);
+// const fruits = ["apples", "oranges", "pears"];
 
-// ul.remove();
-// ul.lastElementChild.remove();
-// ul.firstElementChild.textContent = "Hello";
-// ul.children[1].innerText = "Brad";
-// ul.lastElementChild.innerHTML = "<h1>Hello</h1>";
+// fruits[3] = "grapes";
 
-// const btn = document.querySelector(".btn");
-// // btn.style.background = "red";
-// btn.addEventListener("mouseover", (e) => {
-//   e.preventDefault();
-//   document.querySelector("#my-form").style.background = "#ccc";
-//   document.querySelector("body").classList.add("bg-dark");
-//   document.querySelector(".items").lastElementChild.innerHTML =
-//     "<h1>Hello</h1>";
-// });
-const myForm = document.querySelector("#my-form");
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const msg = document.querySelector(".msg");
-const userList = document.querySelector("#users");
+// fruits.push("mangos");
 
-myForm.addEventListener("submit", onsubmit);
+// console.log(fruits);
+// const fruits = ["apples", "oranges", "pears"];
 
-function onsubmit(e) {
-  e.preventDefault();
+// fruits[3] = "grapes";
 
-  if (nameInput.value === "" || emailInput.value === "") {
-    msg.classList.add("error");
-    msg.innerHTML = "Please enter all fields";
+// fruits.push("mangos");
+// fruits.unshift("strawberries");
 
-    setTimeout(() => msg.remove(), 3000);
-  } else {
-    const li = document.createElement("li");
-    li.appendChild(
-      document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
-    );
+// console.log(fruits);
 
-    userList.appendChild(li);
-    nameInput.value = "";
-    emailInput.value = "";
+// const person = {
+//     firstName: "Tyler",
+//     lastName: "Young",
+//     age: 35,
+//     hobbies: ["music", "movies", "video games"],
+//     address: {
+//       street: "50 main st",
+//       city: "Houston",
+//       state: "TX",
+//     },
+//   };
+
+//   const {
+//     firstName,
+//     lastName,
+//     address: { city },
+//   } = person;
+//   console.log(city);
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+  }
+
+  getBirthYear() {
+    return this.dob.getFullYear();
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
   }
 }
+
+const person1 = new Person("Tyler", "Young", "9-22-1989");
+const person2 = new Person("Tyny", "Anng", "7-02-1976 12:30:15");
+console.log(person2.getFullName());
+console.log(person1);
